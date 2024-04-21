5d1c3b297a71023faaa32b8483c68a038fc1f47c
Ruhadaam <avuclariminicinde@gmail.com>
2024-04-21 17:41:21 +0300

    feature: added fab and custom components

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { useCallback } from 'react';
-import { View,StyleSheet, Text, Image } from "react-native";
-import { FontAwesome, FontAwesome5 } from "@expo/vector-icons"; 
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { LinearGradient } from 'expo-linear-gradient';
+import { useCallback } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { LinearGradient } from "expo-linear-gradient";
+import Customer from "./Components/customer";
+import Fab from "./Components/Fab";
 
 
 SplashScreen.preventAutoHideAsync();
 
-
-
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
-    'Benny-Blanco': require('./assets/fonts/BENNB___.ttf'),
+    "Benny-Blanco": require("./assets/fonts/BENNB___.ttf"),
   });
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded || fontError) {
@@ -26,93 +32,18 @@ export default function App() {
   }
 
 
-    const chats = [
-      {
-        id: 1,
-        name: "Ramle Yatçı",
-        message: "24.05.2024",
-        kalan: "100",
-      },
-      {
-        id: 2,
-        name: "Ayşe Demir",
-        message: "28.05.2024",
-        kalan: "250",
-      },
-    
-    ];
-  
-
   return (
     <LinearGradient
-    style={{ flex: 1 }}
-    colors={['#fbc2eb', '#a6c1ee']} start={{ x: 0.5, y: 0 }} end={{ x: 0.5, y: 1 }}
-  >
-    <View className="flex-1 pt-16 " onLayout={onLayoutRootView}>
-   
-    <View className="flex-row  justify-center">
-      <Text style={styles.font} className="  w-full text-center ">TasarımKidsWorld</Text>
-    </View>
-    {chats.map((item) => (
-      <View style={styles.box} 
-        key={item.id}
-        className="flex-row px-3 items-center border-b border-gray-300 py-5 rounded-xl  bg-white m-5"
-      >
-        <View className="flex-1 justify-between">
-          <View className="flex-row justify-between items-center ">
-            <Text className="font-bold">{item.name}</Text>
-            <View className="flex-row space-x-3">
-              <FontAwesome
-                name="trash"
-                color="red"
-                size={24}
-                className="mr-2 text-gray-400"
-              />
-              <FontAwesome5
-                color="orange"
-                name="info-circle"
-                size={24}
-                className=""
-              />
-            </View>
-          </View>
-          <View className=" flex-row justify-between items-center pt-3">
-            <Text className=""> <Text className="font-extrabold">Kalan: </Text>{item.kalan}TL</Text>
-
-            <Text className="text-gray-600">
-              <Text className="font-extrabold">Teslim: </Text>
-              {item.message}
-            </Text>
-          </View>
-        </View>
-      </View>
-    ))}
-
-
-     
-  </View>
-  </LinearGradient>
-  );
+      style={{ flex: 1 }}
+      colors={["#fbc2eb", "#a6c1ee"]}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 1 }}
+    >
+      <ScrollView>
+      <Customer/>
+      <Fab/>
+      </ScrollView>
   
+    </LinearGradient>
+  );
 }
-const styles = StyleSheet.create({
-  font:{
-    fontFamily:'Benny-Blanco',
-    fontSize:40
-  },
-
-  box: {
-    shadowColor: "#000",
-shadowOffset: {
-	width: 0,
-	height: 8,
-},
-shadowOpacity: 0.44,
-shadowRadius: 10.32,
-
-elevation: 16,
-  },
-});
-
-
-
---
+++ Components/customer.js
@@ -0,0 +1,135 @@
+import {
+    View,
+    StyleSheet,
+    Text,
+    ScrollView,
+    TouchableOpacity,
+  } from "react-native";
+  import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+
+const Customer = () => {
+    const chats = [
+        {
+            id: 1,
+            name: "Ramle Yatçı",
+            message: "24.05.2024",
+            kalan: "100",
+          },
+          {
+            id: 2,
+            name: "Ayşe Demir",
+            message: "28.05.2024",
+            kalan: "250",
+          },
+          {
+            id: 3,
+            name: "Ayşe Demir",
+            message: "28.05.2024",
+            kalan: "250",
+          },
+          {
+            id: 4,
+            name: "Ayşe Demir",
+            message: "28.05.2024",
+            kalan: "250",
+          },
+          {
+            id: 5,
+            name: "Ayşe Demir",
+            message: "28.05.2024",
+            kalan: "250",
+          },
+          {
+            id: 6,
+            name: "Ayşe Demir",
+            message: "28.05.2024",
+            kalan: "250",
+          },
+          {
+            id: 7,
+            name: "Ayşe Demir",
+            message: "28.05.2024",
+            kalan: "250",
+          },
+    ];
+  return (
+    <View className="flex-1 pt-16">
+    <View className="flex-row  justify-center">
+      <Text style={styles.font} className="  w-full text-center ">
+        TasarımKidsWorld
+      </Text>
+    </View>
+    <View style={{ height: 1 }} className=" bg-gray-800 w-10/12 ml-10 " />
+    {chats.map((item) => (
+      <View
+        style={styles.box}
+        key={item.id}
+        className="flex-row px-8 items-center border-b border-gray-300 py-8 rounded-xl  bg-white m-5"
+      >
+        <View className="flex-1 justify-between">
+          <View className="flex-row justify-between items-center ">
+            <Text className="font-bold text-lg">{item.name}</Text>
+            <View className="flex-row space-x-3">
+              <TouchableOpacity>
+              <FontAwesome
+                name="trash"
+                color="red"
+                size={24}
+                className="mr-2 text-gray-400"
+              />
+              </TouchableOpacity>
+              <TouchableOpacity>
+              <FontAwesome5
+                color="orange"
+                name="edit"
+                size={24}
+                className=""
+              />
+              </TouchableOpacity> 
+            </View>
+          </View>
+          <View className=" flex-row justify-between items-center pt-3">
+            <Text className="">
+              {" "}
+              <Text className="font-extrabold">Kalan: </Text>
+              {item.kalan}TL
+            </Text>
+
+            <Text className="text-gray-600">
+              <Text className="font-extrabold">Teslim: </Text>
+              {item.message}
+            </Text>
+          </View>
+        </View>
+      </View>
+    ))}
+
+
+  </View>
+
+  )
+}
+const styles = StyleSheet.create({
+    font: {
+      fontFamily: "Benny-Blanco",
+      fontSize: 40,
+    },
+  
+    box: {
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 8,
+      },
+      shadowOpacity: 0.44,
+      shadowRadius: 10.32,
+  
+      elevation: 16,
+    },
+  
+  
+  
+  });
+
+export default Customer;
+
